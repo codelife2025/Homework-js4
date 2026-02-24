bab1f3e2e211953805283e8f9f0bfac850f3fba7
spartak <spartaksmail.2@gmail.com>
2026-02-24 14:00:24 +0400

    Delete davaleba4 directory

--- davaleba4/main.js
+++
@@ -1,9 +0,0 @@
-let age = 19;
-
-if(typeof age !== "number" || isNaN(age)){ 
-    console.log("მითითებული ასაკი არასწორია!");// ჯერ ვამოწმებთ არის თუ არა რიცხვი
-}else if(age < 18){
-    console.log("თქვენ არ შეგიძლიათ მართვის მოწმობის აღება");
-}else{
-    console.log("თქვენ შეგიძლიათ");
-}
